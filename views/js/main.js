393eda49f73cf671df981405818f5e9ecbaf950f
igor-provotorov <garynya@yandex.ru>
2019-05-19 18:02:15 +0300

    feat: optimize rendering random pizza in main.js

--- views/js/main.js
+++ views/js/main.js
@@ -506,15 +506,19 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function () {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  var slidingPizzaDiv = document.querySelector("#movingPizzas1");
+  slidingPizzaDiv.display = 'none';
+
+  for (var i = 0; i < 35; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza_mini.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    slidingPizzaDiv.appendChild(elem);
   }
+  slidingPizzaDiv.display = 'block';
   updatePositions();
 });
